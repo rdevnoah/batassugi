e59c7a56c7c74bb840427d245f2d4d8f15d61bff
Noah <zzagam2@gmail.com>
2018-05-16 19:56:42 +0900

    RecruitVo 변경 @김영호
    RecruitVo에 구매자가 볼 수 있는 cropsList의 경우, FarmVo가 필요한데 has a 관계가 설정 되어 있지
    않았음. RecruitVo에 FarmVo를 has a 시키는 것으로 변

--- batassugi/src/test/java/org/spider/batassugi/service/RecruitServiceTest.java
+++ batassugi/src/test/java/org/spider/batassugi/service/RecruitServiceTest.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.spider.batassugi.model.dao.seller.RecruitDaoIf;
+import org.spider.batassugi.model.vo.seller.FarmVo;
 import org.spider.batassugi.model.vo.seller.RecruitVo;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,8 +19,8 @@ public class RecruitServiceTest {
   @Test
   public void recruitServiceTest() {
     
-    RecruitVo recruitVo = new RecruitVo(0,1, "대여", null, "2018-06-22", 1000,
-        "대여모집합니다. 잘 관리해드려요 많이 참여하세요", "모집중", 20, null);
+    RecruitVo recruitVo = new RecruitVo(0,new FarmVo(1, null, null, null, null, null, null), "대여", null, "2018-06-22", 1000,
+        "대여모집합니다. 잘 관리해드려요 많이 참여하세요", "모집중", 10, null);
     recruitDao.registerRecruit(recruitVo);
     
     
